a6e3d0f2920800f9fc412c914b27305cab6ee334
ichin23 <pedroferreiramanoel@gmail.com>
2023-01-18 10:23:37 -0300

    esconder senha

--- lib/modules/login/pages/cadastro.dart
+++ lib/modules/login/pages/cadastro.dart
@@ -12,6 +12,8 @@ class CadastroPage extends StatefulWidget {
 
 class _CadastroPageState extends State<CadastroPage> {
   CadastroController controller = CadastroController();
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  bool escondeSenha = true;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -26,76 +28,36 @@ class _CadastroPageState extends State<CadastroPage> {
             child: Container(
               height: size.height,
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: size.width * 0.5,
-                    child: Image.asset(
-                      "src/images/coruja_login.png",
+              child: Form(
+                key: _form,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.5,
+                      child: Image.asset(
+                        "src/images/coruja_login.png",
+                      ),
                     ),
-                  ),
-                  const Text(
-                    "ABNTPLAY",
-                    style: TextStyle(
-                        fontFamily: "Righteous", color: roxo, fontSize: 40),
-                  ),
-                  const SizedBox(height: 10),
-                  Hero(
-                    tag: "email",
-                    child: Material(
-                      child: TextFormField(
-                          controller: controller.email,
-                          style: const TextStyle(
-                              fontFamily: "PassionOne", fontSize: 20),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide:
-                                    const BorderSide(color: lilas, width: 3)),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide:
-                                    const BorderSide(color: lilas, width: 3)),
-                            label: const Text("Email"),
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            hintText: "email@example.com",
-                          )),
+                    const Text(
+                      "ABNTPLAY",
+                      style: TextStyle(
+                          fontFamily: "Righteous", color: roxo, fontSize: 40),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                      controller: controller.nome,
-                      style: const TextStyle(
-                          fontFamily: "PassionOne", fontSize: 20),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide:
-                                const BorderSide(color: lilas, width: 3)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide:
-                                const BorderSide(color: lilas, width: 3)),
-                        suffixIcon: const Icon(
-                          Icons.remove_red_eye,
-                          color: roxo,
-                        ),
-                        label: const Text("Nome"),
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                      )),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Hero(
-                      tag: "senha",
+                    const SizedBox(height: 10),
+                    Hero(
+                      tag: "email",
                       child: Material(
                         child: TextFormField(
-                            controller: controller.senha,
+                            controller: controller.email,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Digite um email válido";
+                              }
+                              return null;
+                            },
                             style: const TextStyle(
                                 fontFamily: "PassionOne", fontSize: 20),
                             decoration: InputDecoration(
@@ -107,73 +69,147 @@ class _CadastroPageState extends State<CadastroPage> {
                                   borderRadius: BorderRadius.circular(12),
                                   borderSide:
                                       const BorderSide(color: lilas, width: 3)),
-                              suffixIcon: const Icon(
-                                Icons.remove_red_eye,
-                                color: roxo,
-                              ),
-                              label: const Text("Senha"),
+                              label: const Text("Email"),
                               floatingLabelBehavior:
                                   FloatingLabelBehavior.never,
+                              hintText: "email@example.com",
                             )),
-                      )),
-                  const SizedBox(height: 40),
-                  Hero(
-                      tag: "botao",
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: primary,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(13)),
-                            fixedSize: Size(size.width * 0.65, 62),
-                            maximumSize: Size(size.width * 0.65, 62)),
-                        child: const Text(
-                          "Cadastrar",
-                          style: TextStyle(
-                              fontFamily: "PassionOne",
-                              fontSize: 32,
-                              color: Colors.white),
-                        ),
-                        onPressed: () async {
-                          try {
-                            await controller.criarConta();
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const HomePage()),
-                                (route) => false);
-                          } catch (e) {
-                            rethrow;
-                          }
-                        },
-                      )),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Já possui conta?",
-                        style: TextStyle(
-                          fontFamily: "PassionOne",
-                          fontSize: 20,
-                        ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(
-                            context,
-                          );
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                        controller: controller.nome,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Digite um nome";
+                          }
+                          return null;
                         },
-                        child: const Text(
-                          "Login",
+                        style: const TextStyle(
+                            fontFamily: "PassionOne", fontSize: 20),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  const BorderSide(color: lilas, width: 3)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  const BorderSide(color: lilas, width: 3)),
+                          suffixIcon: const Icon(
+                            Icons.remove_red_eye,
+                            color: roxo,
+                          ),
+                          label: const Text("Nome"),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                        )),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Hero(
+                        tag: "senha",
+                        child: Material(
+                          child: TextFormField(
+                              controller: controller.senha,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Digite uma senha";
+                                }
+                                return null;
+                              },
+                              style: const TextStyle(
+                                  fontFamily: "PassionOne", fontSize: 20),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                    borderSide: const BorderSide(
+                                        color: lilas, width: 3)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                    borderSide: const BorderSide(
+                                        color: lilas, width: 3)),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      escondeSenha = !escondeSenha;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    escondeSenha
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                    color: roxo,
+                                  ),
+                                ),
+                                label: const Text("Senha"),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.never,
+                              ),
+                              obscureText: escondeSenha),
+                        )),
+                    const SizedBox(height: 40),
+                    Hero(
+                        tag: "botao",
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: primary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13)),
+                              fixedSize: Size(size.width * 0.65, 62),
+                              maximumSize: Size(size.width * 0.65, 62)),
+                          child: const Text(
+                            "Cadastrar",
+                            style: TextStyle(
+                                fontFamily: "PassionOne",
+                                fontSize: 32,
+                                color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            try {
+                              if (_form.currentState!.validate()) {
+                                await controller.criarConta();
+                                Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const HomePage()),
+                                    (route) => false);
+                              }
+                            } catch (e) {
+                              rethrow;
+                            }
+                          },
+                        )),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Já possui conta?",
                           style: TextStyle(
-                              fontFamily: "PassionOne",
-                              fontSize: 20,
-                              color: primary),
+                            fontFamily: "PassionOne",
+                            fontSize: 20,
+                          ),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(
+                              context,
+                            );
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontFamily: "PassionOne",
+                                fontSize: 20,
+                                color: primary),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
--- lib/modules/login/pages/login.dart
+++ lib/modules/login/pages/login.dart
@@ -14,6 +14,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   LoginController controller = LoginController();
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  bool escondeSenha = true;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -24,127 +26,156 @@ class _LoginPageState extends State<LoginPage> {
           child: Container(
             height: size.height,
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: size.width * 0.5,
-                  child: Image.asset(
-                    "src/images/coruja_login.png",
+            child: Form(
+              key: _form,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: size.width * 0.5,
+                    child: Image.asset(
+                      "src/images/coruja_login.png",
+                    ),
                   ),
-                ),
-                const Text(
-                  "ABNTPLAY",
-                  style: TextStyle(
-                      fontFamily: "Righteous", color: roxo, fontSize: 40),
-                ),
-                const SizedBox(height: 10),
-                Hero(
-                  tag: "email",
-                  child: Material(
-                    child: TextFormField(
-                        controller: controller.email,
-                        style: const TextStyle(
-                            fontFamily: "PassionOne", fontSize: 20),
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide:
-                                  const BorderSide(color: lilas, width: 3)),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide:
-                                  const BorderSide(color: lilas, width: 3)),
-                          label: const Text("Email"),
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          hintText: "email@example.com",
-                        )),
+                  const Text(
+                    "ABNTPLAY",
+                    style: TextStyle(
+                        fontFamily: "Righteous", color: roxo, fontSize: 40),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Hero(
-                  tag: "senha",
-                  child: Material(
-                    child: TextFormField(
-                        controller: controller.senha,
-                        style: const TextStyle(
-                            fontFamily: "PassionOne", fontSize: 20),
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide:
-                                  const BorderSide(color: lilas, width: 3)),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide:
-                                  const BorderSide(color: lilas, width: 3)),
-                          suffixIcon: const Icon(
-                            Icons.remove_red_eye,
-                            color: roxo,
+                  const SizedBox(height: 10),
+                  Hero(
+                    tag: "email",
+                    child: Material(
+                      child: TextFormField(
+                          controller: controller.email,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Digite um email válido";
+                            }
+                            return null;
+                          },
+                          style: const TextStyle(
+                              fontFamily: "PassionOne", fontSize: 20),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: lilas, width: 3)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: lilas, width: 3)),
+                            label: const Text("Email"),
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            hintText: "email@example.com",
+                          )),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Hero(
+                    tag: "senha",
+                    child: Material(
+                      child: TextFormField(
+                          controller: controller.senha,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Informe sua senha";
+                            }
+                            return null;
+                          },
+                          style: const TextStyle(
+                              fontFamily: "PassionOne", fontSize: 20),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: lilas, width: 3)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: lilas, width: 3)),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  escondeSenha = !escondeSenha;
+                                });
+                              },
+                              icon: Icon(
+                                escondeSenha
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: roxo,
+                              ),
+                            ),
+                            label: const Text("Senha"),
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
                           ),
-                          label: const Text("Senha"),
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                        )),
+                          obscureText: escondeSenha),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 40),
-                Hero(
-                  tag: "botao",
-                  child: TextButton(
+                  const SizedBox(height: 40),
+                  Hero(
+                    tag: "botao",
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: primary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13)),
+                        fixedSize: Size(size.width * 0.65, 62),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                            fontFamily: "PassionOne",
+                            fontSize: 32,
+                            color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        try {
+                          if (_form.currentState!.validate()) {
+                            await controller.login();
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()),
+                                (route) => false);
+                          }
+                        } catch (e) {
+                          debugPrint(e.toString());
+                        }
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: primary,
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(13)),
+                          borderRadius: BorderRadius.circular(13),
+                          side: const BorderSide(color: primary)),
+                      // padding: EdgeInsets.symmetric(
+                      //     horizontal: size.width * 0.25, vertical: 10),
                       fixedSize: Size(size.width * 0.65, 62),
                     ),
                     child: const Text(
-                      "Login",
+                      "Cadastre-se",
                       style: TextStyle(
                           fontFamily: "PassionOne",
                           fontSize: 32,
-                          color: Colors.white),
+                          color: primary),
                     ),
-                    onPressed: () async {
-                      try {
-                        await controller.login();
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()),
-                            (route) => false);
-                      } catch (e) {
-                        debugPrint(e.toString());
-                      }
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const CadastroPage()));
                     },
-                  ),
-                ),
-                const SizedBox(height: 40),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13),
-                        side: const BorderSide(color: primary)),
-                    // padding: EdgeInsets.symmetric(
-                    //     horizontal: size.width * 0.25, vertical: 10),
-                    fixedSize: Size(size.width * 0.65, 62),
-                  ),
-                  child: const Text(
-                    "Cadastre-se",
-                    style: TextStyle(
-                        fontFamily: "PassionOne", fontSize: 32, color: primary),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const CadastroPage()));
-                  },
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
