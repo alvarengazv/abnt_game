a36c3f671bf737afc0bfe2fce0303923b3f7836e
ichin23 <pedroferreiramanoel@gmail.com>
2023-02-07 08:50:19 -0300

    imagens na tela de "aula" e tela pós aula

--- lib/shared/components/normas/subtopicos.dart
+++ lib/shared/components/normas/subtopicos.dart
@@ -1,14 +1,16 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class TelaSubtopicos extends StatefulWidget {
   final String titulo;
   final Widget corpo;
   final String descricao;
 
-  const TelaSubtopicos({super.key, required this.titulo, required this.corpo, required this.descricao});
+  const TelaSubtopicos(
+      {super.key,
+      required this.titulo,
+      required this.corpo,
+      required this.descricao});
 
   @override
   State<TelaSubtopicos> createState() => _TelaSubtopicosState();
@@ -18,52 +20,55 @@ class _TelaSubtopicosState extends State<TelaSubtopicos> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-              Container(
-                width: size.width,
-                child: Text(
-                  widget.titulo,
-                  style: TextStyle(
-                    fontFamily: "PassionOne",
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: size.width,
+            child: Text(
+              widget.titulo,
+              style: const TextStyle(
+                fontFamily: "PassionOne",
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            width: size.width,
+            constraints: BoxConstraints(minHeight: size.height * 0.4),
+            padding: const EdgeInsets.only(top: 30),
+            child: Align(
+              alignment: Alignment.center,
+              child: widget.corpo,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              width: size.width * 0.9,
+              height: size.height * 0.19,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
                   ),
-                  textAlign: TextAlign.center,
+                  borderRadius: BorderRadius.circular(10)),
+              child: AutoSizeText(
+                widget.descricao,
+                style: const TextStyle(
+                  fontFamily: "Montserrat",
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
                 ),
+                textAlign: TextAlign.justify,
               ),
-              Container(
-                width: size.width,
-                padding: EdgeInsets.only(top: 30),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: widget.corpo,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  width: size.width * 0.9,
-                  height: size.height * 0.19,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: AutoSizeText(
-                    widget.descricao,
-                    style: TextStyle(
-                      fontFamily: "Montserrat",
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-      ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
